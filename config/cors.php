45d1540fdf0904a8f9e71b871c4b893df204eab6
Thwin Htoo Aung <htoohtoo.mdy.mmr@gmail.com>
2024-08-02 08:40:01 +0700

    asdf

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://www.championmaung.com'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
